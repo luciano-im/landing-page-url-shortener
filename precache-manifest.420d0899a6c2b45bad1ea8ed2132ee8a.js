4d7e3c0cbca11b4d176883381b1ba67bfe478d53
Luciano Muñoz <luchisds@gmail.com>
2020-06-24 22:05:04 -0300

    Updates

---
+++ precache-manifest.420d0899a6c2b45bad1ea8ed2132ee8a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e349be56a4ab90e7bf9a4b15a3dbc00",
+    "url": "/landing-page-url-shortener/index.html"
+  },
+  {
+    "revision": "2a99dd5dce8dad0606bd",
+    "url": "/landing-page-url-shortener/static/css/main.6f62275c.chunk.css"
+  },
+  {
+    "revision": "cdd65f0d2dd5b97ced45",
+    "url": "/landing-page-url-shortener/static/js/2.61b9046e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing-page-url-shortener/static/js/2.61b9046e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a99dd5dce8dad0606bd",
+    "url": "/landing-page-url-shortener/static/js/main.3f15ace9.chunk.js"
+  },
+  {
+    "revision": "f67c291d0d0c8b650600",
+    "url": "/landing-page-url-shortener/static/js/runtime-main.8ffa0525.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/landing-page-url-shortener/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/landing-page-url-shortener/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/landing-page-url-shortener/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/landing-page-url-shortener/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/landing-page-url-shortener/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/landing-page-url-shortener/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/landing-page-url-shortener/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/landing-page-url-shortener/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/landing-page-url-shortener/static/media/logo.6387840f.svg"
+  }
+]);
